c7851a21cb144cc09212788a46b795b616d29bdd
SATRIA BAGUS ANJAYA <satriabagusanjaya@gmail.com>
2026-09-20 22:21:52 +0700

    test: correct noncenter anchor rotation case

--- src/features/preview/canvasManipulation.test.ts
+++ src/features/preview/canvasManipulation.test.ts
@@ -88,8 +88,8 @@ describe("canvas manipulation", () => {
       transformFromPointer(
         "rotate",
         base,
-        { x: 100, y: 0 },
-        { x: 0, y: 100 },
+        { x: 300, y: 0 },
+        { x: 200, y: 100 },
         { left: 0, top: 0, width: 200, height: 200 },
         { x: 1, y: 0 },
       ).rotation,
